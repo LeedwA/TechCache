5a00a0a9274ca0f92ac6ec5014fb190ed2bad875
jinzheng519 <jinzheng519@163.com>
2016-07-26 15:17:27 +0800

    Initial commit

---
+++ app/src/main/java/cache/ecar/com/ecarcache/AboutActivity.java
@@ -0,0 +1,15 @@
+package cache.ecar.com.ecarcache;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+
+	}
+
+}
